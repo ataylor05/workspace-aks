54673327c134da80aebc5125d1c98fe922148d25
Allan Taylor <allan@startmail.com>
2021-11-05 07:48:36 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,11 +10,4 @@ provider "azurerm" {
   features {}
 }
 
-provider "kubernetes" {
-  host                   = module.aks.aks_kube_admin_config.0.host
-  username               = module.aks.aks_kube_admin_config.0.username
-  password               = module.aks.aks_kube_admin_config.0.password
-  client_certificate     = base64decode(module.aks.aks_kube_admin_config.0.client_certificate)
-  client_key             = base64decode(module.aks.aks_kube_admin_config.0.client_key)
-  cluster_ca_certificate = base64decode(module.aks.aks_kube_admin_config.0.cluster_ca_certificate)
-}
+
