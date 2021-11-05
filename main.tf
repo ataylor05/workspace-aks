1c3fe27241753bdf8038ecb4bf32e20b064541a2
Allan Taylor <allan@startmail.com>
2021-11-04 22:09:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,72 +1,34 @@
-data "azurerm_subnet" "k8s_subnet" {
-  name                 = var.aks_subnet_name
-  virtual_network_name = var.aks_vnet_name
-  resource_group_name  = var.aks_vnet_rg
+locals {
+  region          = "EastUS2"
+  environment_tag = "Dev"
 }
 
-resource "azurerm_kubernetes_cluster" "k8s" {
-    name                   = var.cluster_name
-    location               = var.region
-    resource_group_name    = var.resource_group
-    kubernetes_version     = var.aks_version
-    dns_prefix             = var.dns_prefix
-    local_account_disabled = var.local_account_disabled
-    sku_tier               = var.sku_tier
-
-    linux_profile {
-      admin_username = var.node_admin_username
-
-      ssh_key {
-          key_data = var.node_admin_ssh_pub_key
-      }
-    }
-
-    addon_profile {
-      kube_dashboard {
-        enabled = var.kube_dashboard
-      }
-      azure_policy {
-        enabled = var.azure_policy
-      }
-    }
-
-    network_profile {
-      network_plugin = "azure"
-      network_policy = var.network_policy
-    }
-
-    default_node_pool {
-      name               = "nodepool"
-      node_count         = 3
-      availability_zones = ["1", "2", "3"]
-      vm_size            = var.cluster_node_vm_size
-      enable_auto_scaling = true
-      min_count           = 3
-      max_count           = var.cluster_auto_scaling_max_nodes
-      os_disk_size_gb     = var.cluster_node_vm_disk_size
-      vnet_subnet_id      = data.azurerm_subnet.k8s_subnet.id
-    }
-
-    identity {
-      type = "SystemAssigned"
-    }
-
-    maintenance_window {
-      allowed {
-        day   = var.maintenance_window_day
-        hours = var.maintenance_window_time_frame
-      }
-    }
-
-    role_based_access_control {
-      enabled = true
-      azure_active_directory {
-        managed                = true
-        admin_group_object_ids = var.aad_admin_group
-      }
-    }
-
-    tags = {
-      Environment = var.environment_tag
-    }
+module "aks" {
+  source                         = "./terraform-azure-kubernetes-service/"
+  # General vars
+  region                         = local.region
+  environment_tag                = local.environment_tag
+  cluster_name                   = "test"
+  resource_group                 = "aks"
+  aks_version                    = "1.20.9"
+  # Network vars
+  aks_subnet_name                = "default"
+  aks_vnet_name                  = "aks"
+  aks_vnet_rg                    = "aks"
+  dns_prefix                     = "ataylor-test"
+  network_policy                 = "calico"
+  # Cluster vars
+  aad_admin_group                = ["02b5231c-637d-4211-b4ef-dc583cac192f"]
+  node_admin_username            = "DTR"
+  node_admin_ssh_pub_key         = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/Ar742uGirB+ofXQdC8/28QucVYXGC4W69A7t0WVflXnGNTox7+7QzGL31sSzpa0pnbVIZ1lHFSYj8sE7EZJgS+eilv5/zV3WF1bTTvB9u/knQPnjXj5zP9Oqr7HSDZKNxEGB3DY2Z2oYD6CYsQ8jJx5NuVy2AUqdmp40gRf7h6Jc7ZaPXp2XLaYU1yjottBDsqiH/ECE7v9YcQOxG/2W6fCOXnFHFesxs9MVlCIS/ld+O72zYoQyaH7N9i1lpnPCTHiNdJ+qezFmmOmvaj3icAdbbnnlZJAbgPKvMrmgnZPiyW9VLN/6TiSDNZi/0tUsZj6tInyZN33dhS7cYjP1zkoqGqWewwViogryFMm4bDwOI4K6S+O/+taGFYmcl18LchPlUdQxrYVgvKvAjAe5vQVCJmw7hv6jqhPF2K8LOLRW7jdY4xPK+jji6OH7t/6BMqJ0cxZk4mkFLEB0uI7dWH22q4OlPJM0UqjDvtxfXir1tGXTZaJuQL7BxVou7UE="
+  cluster_node_vm_disk_size      = 1024
+  cluster_node_vm_size           = "Standard_B2s"
+  cluster_auto_scaling_max_nodes = 10
+  local_account_disabled         = false
+  azure_policy                   = false
+  sku_tier                       = "Free"
+  kube_dashboard                 = false
+  # Maintenance window
+  maintenance_window_day         = "Saturday"
+  maintenance_window_time_frame  = [23,0,1,2,3]
 }
