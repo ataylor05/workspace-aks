5c5b60e4cd29e6c19c0f4ee2f499a76082086f15
Allan Taylor <allan@startmail.com>
2021-11-04 21:48:43 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.k8s.name
+}
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.k8s.id
+}
+
+output "aks_cluster_fqdn" {
+  value = azurerm_kubernetes_cluster.k8s.fqdn
+}
+
+output "aks_node_resource_group" {
+  value = azurerm_kubernetes_cluster.k8s.node_resource_group
+}
+
+output "aks_identity_principal_id" {
+  value = azurerm_kubernetes_cluster.k8s.identity[0].principal_id
+}
+
+output "aks_kube_admin_config_raw" {
+  value = azurerm_kubernetes_cluster.k8s.kube_admin_config_raw
+}
